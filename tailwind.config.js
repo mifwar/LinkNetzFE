acbef55e4b2dd8a64b8ec90de323530224b276a1
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2023-03-10 23:02:10 +0700

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,17 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      Lato: ["Lato", "sans-serif"],
+      Poppins: ["Poppins", "sans-serif"],
+      Mono: ["Roboto Mono", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
