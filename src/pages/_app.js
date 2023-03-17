5936eee71f759cf918529c7e25fb9ad5e6534999
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2023-03-17 08:00:48 +0700

    add fetchUser

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,8 @@ export const TokenContext = createContext();
 
 export default function App({ Component, pageProps }) {
   const [token, setToken] = useState("");
+  const [name, setName] = useState("");
+  const [isTokenLoading, setIsTokenLoading] = useState(true);
 
   useEffect(() => {
     const fetchToken = async () => {
@@ -21,8 +23,11 @@ export default function App({ Component, pageProps }) {
           }
         );
         const json = await response.json();
+
         if (json.token) {
           setToken(json.token);
+        } else {
+          setIsTokenLoading(false);
         }
       } catch {
         console.log("unable to contact the server");
@@ -32,8 +37,39 @@ export default function App({ Component, pageProps }) {
     fetchToken();
   }, []);
 
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/user`,
+          {
+            method: "GET",
+            credentials: "include",
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          }
+        );
+        const json = await response.json();
+
+        console.log("json user: ", json);
+
+        if (json.name) {
+          setName(json.name);
+          setIsTokenLoading(false);
+        } else {
+          setToken("");
+        }
+      } catch {
+        console.log("unable to contact the server");
+      }
+    };
+
+    if (token) fetchUser();
+  }, [token]);
+
   return (
-    <TokenContext.Provider value={{ token, setToken }}>
+    <TokenContext.Provider value={{ token, setToken, isTokenLoading, name }}>
       <Component {...pageProps} />
     </TokenContext.Provider>
   );
