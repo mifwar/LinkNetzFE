37ce5231f9ecc73ed2b65da2f55a65d9761ff847
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2023-03-17 08:01:19 +0700

    remove fetchUser from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,69 +2,18 @@ import { useContext, useState, useEffect } from "react";
 import { TokenContext } from "./_app";
 import withAuth from "../utils/utils";
 
-import Sidebar from "../components/Sidebar";
-
-import Link from "next/link";
+import Navbar from "../components/navbar/Navbar";
+import Sidebar from "../components/sidebar/Sidebar";
 
 const Home = () => {
-  const { token, setToken } = useContext(TokenContext);
-  // const [userEmail, setUserEmail] = useState("");
-  const [name, setName] = useState("");
-
-  const fetchUser = async () => {
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/user`,
-        {
-          method: "GET",
-          credentials: "include",
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        }
-      );
-      const json = await response.json();
-      // setUserEmail(json.email);
-      json.name ? setName(json.name) : setToken("");
-    } catch {
-      console.log("unable to contact the server");
-    }
-  };
-
-  useEffect(() => {
-    if (token) fetchUser();
-  }, [token]);
+  const { token, name } = useContext(TokenContext);
 
   return (
     <div className="bg-slate-50">
-      {token ? (
-        <div className="flex flex-row justify-between">
+      {token && (
+        <div className="flex flex-col h-screen">
+          <Navbar name={name} />
           <Sidebar />
-          <p className="float-right">welcome {name}</p>
-          <button
-            onClick={async () => {
-              const response = await fetch(
-                `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/logout`,
-                {
-                  credentials: "include",
-                }
-              );
-
-              const result = await response.json();
-
-              if (result) setToken("");
-            }}
-          ></button>
-          <Link className="mx-3" href="/auth/login">
-            Logout
-          </Link>
-        </div>
-      ) : (
-        <div>
-          <button>
-            <p>access denied</p>
-            <Link href="/auth/login">Login</Link>
-          </button>
         </div>
       )}
     </div>
