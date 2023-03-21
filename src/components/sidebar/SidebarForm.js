bde9007bdf62498ab94d2b622de4bcc791ab823b
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2023-03-21 18:19:06 +0700

    finalize the SidebarForm

--- src/components/sidebar/SidebarForm.js
+++ src/components/sidebar/SidebarForm.js
@@ -1,21 +1,59 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import data from "@emoji-mart/data";
 import Picker from "@emoji-mart/react";
 
 import { FaCaretRight } from "react-icons/fa";
 
-const SidebarForm = ({ label }) => {
+import { TokenContext } from "../../pages/_app";
+
+const SidebarForm = ({ label, showModal, onSubmitDone }) => {
   const [emoji, setEmoji] = useState("📁");
   const [emojiPicker, setEmojiPicker] = useState(false);
+  const [name, setName] = useState("");
+  const [countName, setCountName] = useState(0);
+  const { token } = useContext(TokenContext);
+
+  const maxLength = 30;
+
+  const handleSubmitForm = async (event) => {
+    event.preventDefault();
+
+    const payload = JSON.stringify({ emoji, name });
+
+    const requestOptions = {
+      method: "POST",
+      credential: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: payload,
+    };
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/add/${label.toLowerCase()}`,
+      requestOptions
+    );
+
+    const res = await response.json();
+
+    if (res) {
+      onSubmitDone();
+    }
+
+    showModal(false);
+  };
 
   return (
-    <div className="flex flex-col items-center px-3 gap-3">
+    <form
+      className="flex flex-col items-center px-3"
+      onSubmit={handleSubmitForm}
+    >
       <div className="flex gap-2 w-full">
         <button
           className="flex flex-col justify-center text-xl hover:bg-slate-200 px-2 rounded-md"
-          onClick={() => {
-            setEmojiPicker(!emojiPicker);
-          }}
+          type="button"
+          onClick={() => setEmojiPicker(!emojiPicker)}
         >
           <div>{emoji}</div>
           <div className="text-sm text-gray-600 absolute rotate-45 translate-x-4 translate-y-3">
@@ -23,7 +61,7 @@ const SidebarForm = ({ label }) => {
           </div>
         </button>
         {emojiPicker && (
-          <div className="absolute translate-x-10 -translate-y-12">
+          <div className="absolute translate-x-10 -translate-y-20">
             <Picker
               data={data}
               onEmojiSelect={(selectedEmoji) => {
@@ -38,14 +76,26 @@ const SidebarForm = ({ label }) => {
         )}
         <input
           className="py-2 px-3 border rounded-md w-full "
-          maxLength={30}
+          maxLength={maxLength}
           placeholder={`${label} Name`}
+          onChange={(event) => {
+            setName(event.target.value);
+            setCountName(event.target.value.length);
+          }}
+          required
         />
       </div>
-      <button className="bg-blue-500 hover:bg-blue-400 text-white rounded-md p-2 w-24">
+      <p className="flex mx-auto mr-0 text-xs text-gray-400">
+        {countName}/{maxLength}
+      </p>
+
+      <button
+        className="bg-blue-500 hover:bg-blue-400 text-white rounded-md p-2 mt-2 w-24"
+        type="submit"
+      >
         Save
       </button>
-    </div>
+    </form>
   );
 };
 
