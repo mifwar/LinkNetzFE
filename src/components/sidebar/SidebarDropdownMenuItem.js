091f912c5cd9321afcc8eff3c57811d91c62278e
Miftahul Anwar <miftahulanwar@ce.student.pens.ac.id>
2023-04-02 22:55:11 +0700

    tidy up code

--- src/components/sidebar/SidebarDropdownMenuItem.js
+++ src/components/sidebar/SidebarDropdownMenuItem.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
-import { AiOutlinePlus, AiOutlineDown, AiOutlineUp } from "react-icons/ai";
-import { BsThreeDotsVertical } from "react-icons/bs";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
+import { AiOutlinePlus, AiOutlineDown, AiOutlineRight } from "react-icons/ai";
 
 import Modal from "../modal/Modal";
+import MenuItem from "./MenuItem";
 import SidebarForm from "./SidebarForm";
 
 const SidebarDropdownMenuItem = ({
@@ -14,21 +16,57 @@ const SidebarDropdownMenuItem = ({
 }) => {
   const [showHidden, setShowHidden] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [showEdit, setShowEdit] = useState();
+  const [showDelete, setShowDelete] = useState();
+
+  const [pathName, setPathName] = useState("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setShowEdit(Array(contents.length).fill(false));
+    setShowDelete(Array(contents.length).fill(false));
+  }, [contents]);
+
+  useEffect(() => {
+    setPathName(decodeURIComponent(router.query.path));
+  }, [router.asPath]);
+
+  useEffect(() => {
+    console.log("showDelete: ", showDelete);
+  }, [showDelete]);
 
   const renderMenuItems = (menuItem) => {
-    return menuItem.map((sub, i) => {
-      return (
-        <button
-          className="group flex gap-2 items-center hover:bg-slate-200 pl-9 pr-3 overflow-ellipsis"
-          key={i}
-        >
-          <p className="">{sub.emoji}</p>
-          <p className="w-full text-left truncate">{sub.name}</p>
-          <button className="invisible group-hover:visible hover:bg-slate-100 p-1 mx-auto mr-0">
-            <BsThreeDotsVertical />
-          </button>
-        </button>
+    const toggleFlag = (currentIndex, currentFlags, setFlags) => {
+      const newState = currentFlags.map((state, idx) =>
+        idx === currentIndex ? !state : false
       );
+      setFlags(newState);
+    };
+
+    const toggleEditFlag = (i) => {
+      toggleFlag(i, showEdit, setShowEdit);
+    };
+
+    const toggleDeleteFlag = (i) => {
+      toggleFlag(i, showDelete, setShowDelete);
+    };
+
+    return menuItem.map((subMenu, i) => {
+      const { id, name, emoji } = subMenu;
+      const props = {
+        pathName,
+        id,
+        name,
+        emoji,
+        isShowEdit: showEdit[i],
+        isShowDelete: showDelete[i],
+        label,
+        onSubmitDone,
+        toggleEditFlag: () => toggleEditFlag(i),
+        toggleDeleteFlag: () => toggleDeleteFlag(i),
+      };
+      return <MenuItem {...props} />;
     });
   };
 
@@ -40,37 +78,40 @@ const SidebarDropdownMenuItem = ({
             label={label}
             showModal={setShowModal}
             onSubmitDone={onSubmitDone}
+            openMenu={() => setShowHidden(true)}
           />
         </Modal>
       )}
-      <button
-        className="flex flex-row items-center gap-2 w-full font-Lato px-3 hover:bg-slate-200 "
-        onClick={() => {
-          setShowHidden(!showHidden);
-        }}
-      >
-        <div>{icon}</div>
-        <div className="flex flex-row w-full">
-          <p className="py-2 ">{title}</p>
-        </div>
-        <div className="mx-auto mr-0">
-          {!showHidden ? <AiOutlineDown /> : <AiOutlineUp />}
-        </div>
-      </button>
-      {showHidden && (
-        <div className="flex flex-col gap-1 ">
-          {renderMenuItems(contents)}
+      <div className="group flex bg-slate-50 hover:bg-slate-200 items-center">
+        <div className="flex-grow">
           <button
-            className="flex flex-row items-center gap-2 text-gray-500 hover:bg-slate-200 w-full px-9"
+            className="flex flex-row items-center gap-2 w-full font-Lato px-3 hover:bg-slate-200 "
             onClick={() => {
-              setShowModal(!showModal);
+              setShowHidden(!showHidden);
             }}
           >
-            <div className="border-2 border-gray-300 rounded-sm text-xs">
-              <AiOutlinePlus />
+            {!showHidden ? <AiOutlineRight /> : <AiOutlineDown />}
+            <div>{icon}</div>
+            <div className="flex flex-row w-full">
+              <p className="py-2 ">{title}</p>
             </div>
-            <p>Add New {label}</p>
           </button>
+        </div>
+        <div className="flex-shrink-0 mx-2">
+          <button
+            className="p-1 z-10 text-sm hover:bg-slate-50 rounded-md"
+            onClick={() => setShowModal(!showModal)}
+          >
+            <AiOutlinePlus />
+          </button>
+        </div>
+      </div>
+      {showHidden && (
+        <div className="flex flex-row w-full relative">
+          <div className="absolute border-r h-full ml-[1.15rem] my-1 border-gray-400"></div>
+          <div className="flex flex-col w-full ">
+            {renderMenuItems(contents)}
+          </div>
         </div>
       )}
     </div>
